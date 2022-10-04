0309b2705bcf9c27d3c637498a285cd88cc13b75
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-04 18:25:51 -0500

    FEAT: wrap the HTML in a HTML element for it to render to the page

--- react_project/react_info_site/script/index.js
+++ react_project/react_info_site/script/index.js
@@ -1,9 +1,20 @@
 function Header(){
     return (
-        <h1>codingBYanthony</h1>
+        <p>
+            <h1>coding<span id="title-el">BY</span>anthony</h1>            
+
+            <h3>react info site</h3>
+
+            <br></br>
+
+            <p>why does it only take a single element?</p>
+
+            <p>it seems you have to set up the DOM like a document to be called but it has to be wrapped in an HTML element</p>
+        </p>
     );
 }
 
+// why can the render method only do one DOM element at a time?
 ReactDOM.render(
         <Header />,
     document.querySelector("#root")
