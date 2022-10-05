9fb7d0743e2ef1eadcb5b8c08743b949dcb8bce0
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-04 20:15:51 -0500

    FEAT: react 18 uses a new render path and language, implementing the changes from here on

--- react_project/react_info_site/script/index.js
+++ react_project/react_info_site/script/index.js
@@ -1,18 +1,7 @@
-/*Challenge: 
-
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
-
 function Header(){
     return (
         <nav>
-            <h1>code<span id="title-el">panda</span></h1>
+            <h1>coding<span id="title-el">BY</span>anthony</h1>
             <ul>
                 <li>Pricing</li>
                 <li>About</li>
@@ -23,7 +12,5 @@ function Header(){
 }
 
 // why can the render method only do one DOM element at a time?
-ReactDOM.render(
-        <Header />,
-    document.querySelector("#root")
-    );
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<Header />);
