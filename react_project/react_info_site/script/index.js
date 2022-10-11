77e004ad8fd311ad095dac0e8c8aab7bc6d8b1f8
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-10 21:16:13 -0500

    CHORE: stlying footer in react

--- react_project/react_info_site/script/index.js
+++ react_project/react_info_site/script/index.js
@@ -2,6 +2,7 @@ function Header(){
     return (
         <header>
             <nav className="nav">
+            <img src="../images/S.A.M.E.Logo.jpg" className="nav-logo"></img>
             <ul className="nav-items">
                 <li>pricing</li>
                 <li>about</li>
