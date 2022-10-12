94eef847eed95a07df81c9b420c235fa27738396
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-11 21:42:19 -0500

    FEAT: inital commit for react act and components

---
+++ react_project_001/App.js
@@ -0,0 +1,11 @@
+import Main from "./components/Main"
+import Nav from "./components/Navbar"
+
+export default function App(){
+    return(
+        <div className="container">
+            <Nav />
+            <Main />
+        </div>
+    )
+}
---
+++ react_project_001/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main(){
+    return(
+    <div>
+        <h1>This is the Main content page</h1>
+    </div>
+    )
+}
