12ac38055733583268d5611cbbc76b6aff6d28e2
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-14 07:06:25 -0500

    CHORE: removed unnecessary files Class components and added Card component

---
+++ react_project/codingBYanthony-robofriends/robofriends/src/Card.js
@@ -0,0 +1,14 @@
+// Card Component
+import React, {Component} from "react"
+import "./index.css"
+
+class Card extends Component{
+    render(){
+        return(
+            <div>
+                I am the Card Component.
+            </div>
+        );
+    }
+}
+export default Card;
