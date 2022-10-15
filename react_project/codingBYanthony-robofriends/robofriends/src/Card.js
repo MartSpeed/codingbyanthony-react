be6c5f5ff508d3598dc58b97370736e066fbc5ea
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-14 22:33:16 -0500

    FEAT: adding fragments to the inner div to remove an erroneous type file and cleaning JSX DOM partionting

--- react_project/codingBYanthony-robofriends/robofriends/src/Card.js
+++ react_project/codingBYanthony-robofriends/robofriends/src/Card.js
@@ -1,12 +1,15 @@
-const Card = (props) => {
+import React from "react";
+
+// using destructuring to just add the prop titles directly
+const Card = ({id, name, username, email}) => {
     return(
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt ="robot" src={`https://robohash.org/${props.id}?200x200`} />
-            <div>
-                <h2>{props.name}</h2>
-                <p>{props.username}</p>
-                <p>{props.email}</p>
-            </div>            
+            <img alt ="robot" src={`https://robohash.org/${id}?200x200`} />
+            <React.Fragment>
+                <h2>{name}</h2>
+                <p>{username}</p>
+                <p>{email}</p>
+            </React.Fragment>            
         </div>
     );
 }
