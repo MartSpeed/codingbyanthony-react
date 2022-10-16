9316b77a94deb1c8380d5bcc715aead8b98ccfe0
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-15 23:06:46 -0500

    FEAT: added card list branch for new component

--- react_project/codingBYanthony-robofriends/robofriends/src/index.js
+++ react_project/codingBYanthony-robofriends/robofriends/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from "./Header";
 import Card from "./Card";
+import CardList from "./CardList";
 import {robots} from "./robots"
 import reportWebVitals from './reportWebVitals';
 import "tachyons";
