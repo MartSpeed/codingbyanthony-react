f07617fee98e8b40413a7ad03481d1eef49b7e4d
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-16 00:00:21 -0500

    CHORE: removed card component from index.js since the card component is the main DOM rendered component

--- react_project/codingBYanthony-robofriends/robofriends/src/index.js
+++ react_project/codingBYanthony-robofriends/robofriends/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from "./Header";
-import Card from "./Card";
 import CardList from "./CardList";
 import {robots} from "./robots"
 import reportWebVitals from './reportWebVitals';
