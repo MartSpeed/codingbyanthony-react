e62e4833a25628d99dd1e128869c501f4effb1e4
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-17 20:46:11 -0500

    ADD: added a div the robofriend header

--- react_project/codingBYanthony-robofriends/robofriends/src/ROBOFRIEND_HEADER.js
+++ react_project/codingBYanthony-robofriends/robofriends/src/ROBOFRIEND_HEADER.js
@@ -3,7 +3,9 @@ import React, {Component} from "react";
 class ROBOFRIENDS_HEADER extends Component{
     render(){
         return(
-            <h1>robofriends</h1>
+            <div>
+                <h1>robofriends</h1>
+            </div>
         )
     }
 }
