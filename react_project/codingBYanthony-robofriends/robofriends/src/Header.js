2cc38b76b68ef24c98233f7f75b68468280a9e4d
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-18 22:44:56 -0500

    ADD: added personal font for title name

--- react_project/codingBYanthony-robofriends/robofriends/src/Header.js
+++ react_project/codingBYanthony-robofriends/robofriends/src/Header.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class Header extends Component{
     render(){
         return(
-            <h1 className="f1 tc">coding<span className="green"><strong>BY</strong></span>anthony</h1>
+            <h1 className="f1 tc cba">coding<span className="green"><strong>BY</strong></span>anthony</h1>
         )
     }
 }
