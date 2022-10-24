790ee64bbf3b65095c9325814fd0ccbe7558b4ca
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-23 19:58:49 -0500

    ADD: map array methods reinforcement practice

---
+++ codingbyanthony-javascript/map_operator/codingbyanthony_map.js
@@ -0,0 +1,19 @@
+const companies = [
+    {name: "company one", category: "finance", start: 1981, end:2004},
+    {name: "company two", category: "retail", start: 1992, end:2008},
+    {name: "company three", category: "auto", start: 1999, end:2007},
+    {name: "company four", category: "retail", start: 1989, end:2010},
+    {name: "company five", category: "technology", start: 2009, end:2014},
+    {name: "company six", category: "finance", start: 1987, end:2010},
+    {name: "company seven", category: "auto", start: 1986, end:1996},
+    {name: "company eight", category: "technology", start: 2011, end:2016},
+    {name: "company nine", category: "finance", start: 1981, end:1989},
+];
+
+const ages = [33,12,20,16,5,54,21,44,61,13,15,45,25,64,32];
+
+// forEach
+// filter
+// map
+// sort
+// reduce
