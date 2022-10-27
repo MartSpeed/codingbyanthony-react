1d65dcec5e7cc15972aec03b25d1a379e117893b
John Anthony <jmanthony.fullsail@gmail.com>
2022-10-26 22:03:32 -0500

    ADD: map through array of monster for DRY code and better learning key values within the map method

--- react_project/react_monster_rolodex/monster_rolodex/src/App.js
+++ react_project/react_monster_rolodex/monster_rolodex/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
@@ -8,31 +8,29 @@ class App extends Component{
     super();
 
     this.state = {
-      name: {firstname: 'John', lastname: 'Anthony'},
-      company: 'codingBYanthony'
+      monster:[
+        {
+          name:'Albedo'
+        },
+        {
+          name:'Oda'
+        },
+        {
+          name:'PowerHawk'
+        },
+        {
+          name:'Mugiwara'
+        }      
+      ]
     }
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi, my name is {this.state.name.firstname} {this.state.name.lastname} and I work for {this.state.company}
-          </p>
-          <button onClick={() => {
-            // the first function is the updater function
-            // the second function is the callback function
-            this.setState(() => {
-              return{
-                name:{firstname:'Albedo', lastname:'Palazzo'}, company:'The Nines'
-              }
-            });
-          }}>
-            Change Name
-          </button>
-        </header>
+      {
+        this.state.monster.map((monsters)=>{return(<h1>{monsters.name}</h1>)})
+      }
       </div>
     )
   }
