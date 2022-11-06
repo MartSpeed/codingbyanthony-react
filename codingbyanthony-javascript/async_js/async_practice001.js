d2a9c761e8e3987c8b4c5cb4b1a10e4828f99f44
John Anthony <jmanthony.fullsail@gmail.com>
2022-11-06 12:56:11 -0600

    FEAT: callback and promises

--- codingbyanthony-javascript/async_js/async_practice001.js
+++ codingbyanthony-javascript/async_js/async_practice001.js
@@ -1,5 +1,44 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
 // *****CALLBACKS*****
 // setTimeout
+
+// setTimeout(() => {
+//     console.log('waited 3 seconds');
+// }, 3000);
+
 // nested setTimeouts
+
+// setTimeout(() => {
+//     console.log('3 third');
+//     setTimeout(() => {
+//         console.log('2 second');
+//         setTimeout(() => {
+//             console.log('1 first');
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
 // button event handler in browser javascript
+
+// const btn;
+// btn.addEventListener('click', () => {
+//     console.log('click asyncg example')
+// })
+
+
 //  error first callback
+// fs.readFile('./test.txt', {encoding: 'utf-8'}, (err, data) => {
+//     console.log(data)
+// })
+
+// *****PROMISES*****
+// create a promise
+
+
+
+// fs readRFile with promises
+
+
+
+// fetch with promises
