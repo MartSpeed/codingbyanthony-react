470bc36da679e95b815f9091f7e79bf5c0ed9ef3
John Anthony <jmanthony.fullsail@gmail.com>
2022-12-04 18:44:45 -0600

    FEAT: socket io practice is complete

--- react_project/sockets_with_reactjs/client/src/App.js
+++ react_project/sockets_with_reactjs/client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import io from 'socket.io-client';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // use this to emit or listen to events
 const socket = io.connect("http://localhost:3001");
@@ -8,11 +8,23 @@ const socket = io.connect("http://localhost:3001");
 
 
 function App () {
+    // Room State
+    const [room, setRoom] = useState("");
+
+    // Message states
+    const [message, setMessage] = useState("");
+    // grabs whatever the user is typing in the input
+    const [messageReceived, setMessageReceived] = useState(""); 
+
+    const joinRoom = () => {
+        if(room !== ""){
+            socket.emit("join_room", room); 
+        }
+    };
     
     const sendMessage = () => {
         // emit message to someone or something
-        socket.emit("send_message", {message: "I like going to the skatig rink, its one of my favorite activities."})
-
+        socket.emit("send_message", { message, room })
     }; // called when button is clicked
 
     // setup the connection to our socket.io server on the backend
@@ -20,19 +32,32 @@ function App () {
     // function that will run, any time an event is thrown to us on the socket.io server
     useEffect(() => {
         socket.on("receive_message", (data) => {
-            alert(data.message)
+            setMessageReceived(data.message)
         })
     })
     return (
         
         <div className="App">
-
-            <input placeholder="Message...">
+            <input
+                placeholder="Room Number..."
+                onChange={(event) => {
+                    setRoom(event.target.value);
+                }}
+                ></input>
+            <input 
+                placeholder="Message..." 
+                onChange={(event) => {
+                    setMessage(event.target.value);
+                }}>
 
             </input>
 
-            <button onClick={sendMessage}> Send Message </button>
+            <button
+                onClick={joinRoom}> Join Room </button>
 
+            <button onClick={sendMessage}> Send Message </button>
+            <h1>Message:</h1>
+            { messageReceived }
         </div>
     );
 }
