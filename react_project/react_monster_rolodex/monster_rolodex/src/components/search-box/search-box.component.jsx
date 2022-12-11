db4bf4f38d842dbb39a0c39a49930e6888476afe
John Anthony <jmanthony.fullsail@gmail.com>
2022-12-10 23:41:21 -0600

    FEAT: searchbox styling and card component styling complete

--- react_project/react_monster_rolodex/monster_rolodex/src/components/search-box/search-box.component.jsx
+++ react_project/react_monster_rolodex/monster_rolodex/src/components/search-box/search-box.component.jsx
@@ -1,11 +1,12 @@
 import { Component } from 'react';
+import './search-box.styles.css'
 
 class SearchBox extends Component{
     render(){
         return(
              <input
-                className={this.props.className}
                 type = 'search'
+                className={` search-box ${this.props.className} `}
                 placeholder={this.props.placeHolder}
                 onChange={this.props.onChangeHandler}           
             />
