eeaa7a754e0b68d380d2c66a2ae92de023c9f131
John Anthony <jmanthony.fullsail@gmail.com>
2022-12-12 21:13:40 -0600

    FEAT: class components to functional components, keep up with the practice #code001/100

--- react_project/react_monster_rolodex/monster_rolodex/src/App.js
+++ react_project/react_monster_rolodex/monster_rolodex/src/App.js
@@ -48,6 +48,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+      <h1 className="app-title">Monster Holodeck</h1>
         <SearchBox
           className='monsters-search-box'            
           placeHolder='search monster...'
