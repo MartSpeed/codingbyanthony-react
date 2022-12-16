cf78f5dd773e998fa21a78e8ed53c0484f631d33
John Anthony <jmanthony.fullsail@gmail.com>
2022-12-16 17:24:38 -0600

    REFACTOR: changed the class component to a functional component, need to practoce class components more

--- react_project/react_monster_rolodex/monster_rolodex/src/App.js
+++ react_project/react_monster_rolodex/monster_rolodex/src/App.js
@@ -3,61 +3,93 @@ import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-import {Component} from 'react';
+import { useState } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
+// A. function component
 
-    this.state = {monster: [], searchField: ''};
-  }
+const App = () => {
 
-  // REACT LIFECYCLE methods: componentDidMount: whatever you write here is when
-  // the component mounts/renders Promise: a promise to eventually have a value
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then((response) => response.json())
-        .then(
-            (users) => this.setState(
-                () => {
-                  // using a function method to return object points to users
-                  return {
-                    monster: users
-                  }
-                }
-            ))
-  }
+  // useState gives back an array of 2 values
+  // assign variables to values inside of an array
+const [searchField, setSearchField] = useState(''); // [value, setValue]
+console.log({ searchField });
 
-  // Optimization to store the method instead of using it as an anonymous function
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField }
-    });
-  };
+const onSearchChange = (event) => {
+  const searchFieldString = event.target.value.toLocaleLowerCase();
+  setSearchField(searchFieldString);
+};
 
-  render() {
-    // console.log('inside of App js');
-
-    const { monster, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monster.filter((monsters) => {
-      return monsters.name.toLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className='App'>
-      <h1 className="app-title">Monster Holodeck</h1>
-        <SearchBox
-          className='monsters-search-box'            
-          placeHolder='search monster...'
-          onChangeHandler={onSearchChange}
-          />
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    )
-  }
+  return (
+          <div className='App'>
+          
+            <h1 className="app-title">Monster Holodeck</h1>
+            
+            <SearchBox
+              className='monsters-search-box'            
+              placeHolder='search monster...'
+              onChangeHandler={onSearchChange}
+              />
+            
+            {/* <CardList monsters={filteredMonsters}/> */}
+          
+          </div>
+        )
 }
+
+// B. class component
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {monster: [], searchField: ''};
+//   }
+
+//   // REACT LIFECYCLE methods: componentDidMount: whatever you write here is when
+//   // the component mounts/renders Promise: a promise to eventually have a value
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then((response) => response.json())
+//         .then(
+//             (users) => this.setState(
+//                 () => {
+//                   // using a function method to return object points to users
+//                   return {
+//                     monster: users
+//                   }
+//                 }
+//             ))
+//   }
+
+//   // Optimization to store the method instead of using it as an anonymous function
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField }
+//     });
+//   };
+
+//   render() {
+//     // console.log('inside of App js');
+
+//     const { monster, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monster.filter((monsters) => {
+//       return monsters.name.toLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className='App'>
+//       <h1 className="app-title">Monster Holodeck</h1>
+//         <SearchBox
+//           className='monsters-search-box'            
+//           placeHolder='search monster...'
+//           onChangeHandler={onSearchChange}
+//           />
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
