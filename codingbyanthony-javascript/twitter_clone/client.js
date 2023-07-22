a98ebf0d1898f0d5f624e16cdc2ce0697e50a8b5
John Anthony <jmanthony.fullsail@gmail.com>
2023-07-22 10:54:34 -0500

    feat: hid the loading feature, need create the conditional to show this when messages are loading

--- codingbyanthony-javascript/twitter_clone/client.js
+++ codingbyanthony-javascript/twitter_clone/client.js
@@ -7,6 +7,11 @@ console.log(`Welcome to the Spiderverse!`)
 // anytime you see document that is client side JS
 const form = document.querySelector('form')
 
+// hide the logo when it thr form is now loading messages
+const loadingElement = document.querySelector('.loading')
+// whent hepage loads, hide the loading animation
+loadingElement.style.display = 'none';
+
 // listen to when the user clicks submit using an event listener
 // grab the data from the textarea
 form.addEventListener('submit', (event) => {
