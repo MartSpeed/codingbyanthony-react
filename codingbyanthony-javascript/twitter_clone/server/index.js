e3ecd070d2da222449f09175a45b182e3888f62d
John Anthony <jmanthony.fullsail@gmail.com>
2023-07-22 11:33:46 -0500

    feat: set up server to respond to client request

--- codingbyanthony-javascript/twitter_clone/server/index.js
+++ codingbyanthony-javascript/twitter_clone/server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (request, response) => [
+    response.json({
+        message: 'You friendly neighborhood Spider-Man!'
+    })
+])
+
 app.listen(5000, () => {
     console.log('Listening on http://loaclhost:5000')
 })
